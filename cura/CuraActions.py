8df667890703fac39e4b48db6eb2c260f44fa481
Allen McAfee <allen@robo3d.com>
2018-09-18 11:01:49 -0700

    Revert "changing link destinations, removing Troubleshooting guide link"
    This reverts commit a2fbfb3a1a535dc8c503a5368fb78ea6f5cbe05a.

--- cura/CuraActions.py
+++ cura/CuraActions.py
@@ -36,12 +36,12 @@ class CuraActions(QObject):
         # Starting a web browser from a signal handler connected to a menu will crash on windows.
         # So instead, defer the call to the next run of the event loop, since that does work.
         # Note that weirdly enough, only signal handlers that open a web browser fail like that.
-        event = CallFunctionEvent(self._openUrl, [QUrl("https://help.robo3d.com/hc/en-us/categories/115000094472-Cura-for-Robo-Desktop")], {})
+        event = CallFunctionEvent(self._openUrl, [QUrl("http://ultimaker.com/en/support/software")], {})
         cura.CuraApplication.CuraApplication.getInstance().functionEvent(event)
 
     @pyqtSlot()
     def openBugReportPage(self) -> None:
-        event = CallFunctionEvent(self._openUrl, [QUrl("http://github.com/robo3d/Cura/issues")], {})
+        event = CallFunctionEvent(self._openUrl, [QUrl("http://github.com/Ultimaker/Cura/issues")], {})
         cura.CuraApplication.CuraApplication.getInstance().functionEvent(event)
 
     ##  Reset camera position and direction to default
